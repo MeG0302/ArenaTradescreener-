67a98144140ebd9ff8be35c1b410ad3d31b9930f
MeG <112762788+MeG0302@users.noreply.github.com>
2025-06-20 03:53:18 +0530

    Update api.js

--- api.js
+++ api.js
@@ -6,9 +6,11 @@ const API_BASE = 'https://api.arena.trade/rpc';
 export const getCoins = () =>
   axios
     .get(`${API_BASE}/group_trades_plus_recent?in_limit=15&in_offset=0`)
-    .then((res) => res.data)
+    .then((res) => {
+      console.log("📡 Arena API response received:", res.data);
+      return res.data;
+    })
     .catch((err) => {
-      console.error("Error fetching coin data:", err);
+      console.error("🚨 Error fetching coin data from Arena:", err);
       throw err;
     });
-
